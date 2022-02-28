ebe4b82aa91f42bfedf1539cb378ac782d5e6e6a
n1ckda <100207451+n1ckda@users.noreply.github.com>
2022-02-28 22:50:22 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aviatrix_segmentation_security_domain" "BU2" {
 }
  
   
- resource "aviatrix_segmentation_security_domain_connection_policy" "BU1_BU2" {
+ resource "aviatrix_segmentation_security_domain_connection_policy" "segmentation_security_domain_connection_policy_1" {
   domain_name_1 = "BU1"
   domain_name_2 = "BU2"
   depends_on    = [aviatrix_segmentation_security_domain.BU1, aviatrix_segmentation_security_domain.BU2]
